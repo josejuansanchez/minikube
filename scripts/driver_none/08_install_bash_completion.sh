3b0d7a96f294511cd2fe7bde7bcc9acdb3220858
José Juan Sánchez <josejuan.sanchez@gmail.com>
2024-02-28 23:14:08 +0100

    Se modifican los scripts de bash completion

--- scripts/driver_none/08_install_bash_completion.sh
+++ scripts/driver_none/08_install_bash_completion.sh
@@ -6,3 +6,11 @@ apt update
 
 # Instalamos el paquete bash-completion
 apt install bash-completion -y
+
+# Configuramos el autocompletado de kubectl
+echo 'source <(kubectl completion bash)' >> /home/$SUDO_USER/.bashrc
+
+# Configuramos el autocompletado de minikube
+echo 'source <(minikube completion bash)' >> /home/$SUDO_USER/.bashrc
+
+source /home/$SUDO_USER/.bashrc 
--- scripts/driver_virtualbox/05_install_bash_completion.sh
+++ scripts/driver_virtualbox/05_install_bash_completion.sh
@@ -8,9 +8,9 @@ apt update
 apt install bash-completion -y
 
 # Configuramos el autocompletado de kubectl
-echo 'source <(kubectl completion bash)' >>~/.bashrc
+echo 'source <(kubectl completion bash)' >> /home/$SUDO_USER/.bashrc
 
 # Configuramos el autocompletado de minikube
-echo 'source <(minikube completion bash)' >>~/.bashrc
+echo 'source <(minikube completion bash)' >> /home/$SUDO_USER/.bashrc
 
-source ~/.bashrc 
+source /home/$SUDO_USER/.bashrc 
