e41961010220b5dfb363c10a973e291070cc397d
José Juan Sánchez <josejuan.sanchez@gmail.com>
2024-04-29 22:54:29 +0200

    Update start_minikube_driver_none.sh

--- scripts/driver_none/start_minikube_driver_none.sh
+++ scripts/driver_none/start_minikube_driver_none.sh
@@ -2,10 +2,10 @@
 set -ex
 
 # Iniciamos minikube con el driver none, cni para la gestión de redes y el plugin calico
-minikube start --driver none --network-plugin cni --cni calico
+#minikube start --driver none --network-plugin cni --cni calico
 
 # Otra opción podría utilizar el plugin de flannel
-#minikube start --driver none --network-plugin cni --cni flannel
+minikube start --driver none --network-plugin cni --cni flannel
 
 # Habilitamos el addon de ingress
 minikube addons enable ingress
